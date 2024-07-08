2912c17481d1c42bd011aed3a0024279aa45bd11
Nicolas GUYOT <nicolas@ngware.fr>
2024-07-08 14:57:40 +0200

    Initial Commit

---
+++ MF2Revisions/MF2RevisionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MF2RevisionsApp.swift
+//  MF2Revisions
+//
+//  Created by Nicolas Guyot on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MF2RevisionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
